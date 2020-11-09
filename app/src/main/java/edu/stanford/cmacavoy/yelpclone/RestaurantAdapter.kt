17756b72488dadeb78c7ec1c30b5b99a2f347883
Clara MacAvoy <cmacavoy@stanford.edu>
2020-11-08 16:25:18 -0800

    Images Loading

--- app/src/main/java/edu/stanford/cmacavoy/yelpclone/RestaurantAdapter.kt
+++ app/src/main/java/edu/stanford/cmacavoy/yelpclone/RestaurantAdapter.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.item_restaurant.view.*
 
 class RestaurantAdapter(val context: Context, val restaurants: List<YelpRestaurant>) :
@@ -13,6 +17,16 @@ class RestaurantAdapter(val context: Context, val restaurants: List<YelpRestaura
     inner class  ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         fun bind(yelpRestaurant: YelpRestaurant) {
             itemView.tvName.text = yelpRestaurant.name
+            itemView.ratingBar.rating = yelpRestaurant.rating.toFloat()
+            itemView.tvNumReviews.text = "${yelpRestaurant.numReviews} Reviews"
+            itemView.tvAddress.text = yelpRestaurant.location.address
+            itemView.tvCategory.text = yelpRestaurant.categories[0].title
+            itemView.tvDistance.text = yelpRestaurant.displayDistance()
+            itemView.tvPrice.text = yelpRestaurant.price
+            Glide.with(context).load(yelpRestaurant.imageURL).apply(RequestOptions().transforms(
+                CenterCrop(), RoundedCorners(20)
+            )).into(itemView.imageView)
+
         }
 
     }
